d6fd493392f206de3fee51d400d9eace69af1a36
taslakhazir1903 <taslakhazir@gmail.com>
2025-06-08 20:09:06 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return <h1>Hazırtaslak Çalışıyor</h1>
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
