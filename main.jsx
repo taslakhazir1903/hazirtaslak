9891d82465c68cb0b40eb5e9f3ca4ca3466b460d
taslakhazir1903 <taslakhazir@gmail.com>
2025-06-08 18:41:24 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return <h1>Hazırtaslak Kira Kontratı Başladı!</h1>
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
