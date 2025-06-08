c32ee7758ab8bbc74e3ea6c3dcbab06cbaae7125
taslakhazir1903 <taslakhazir@gmail.com>
2025-06-09 00:15:58 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+function App() {
+  return <h1>Hazırtaslak Çalışıyor</h1>
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
