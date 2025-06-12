c23d9097d22f645e7cfce83662b6ac78a5f1450a
taslakhazir1903 <taslakhazir@gmail.com>
2025-06-13 01:10:40 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-/** @type {import('vite').UserConfig} */
-export default {
+export default defineConfig({
   plugins: [react()],
-  build:
+  build: {
+    outDir: 'dist',
+  }
+});
